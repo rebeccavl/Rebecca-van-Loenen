c90e6bb67ddde939dddf15395cf6a6e7696c48f5
Rebeccavl <rebeccavl@outlook.com>
2017-03-22 15:58:49 -0400

    homepage

--- app/containers/About/index.js
+++ app/containers/About/index.js
@@ -6,19 +6,51 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
-
-export class About extends React.PureComponent {
+import {Link} from "react-router";
+export default class About extends React.PureComponent {
   render() {
+
+    const mainStyle={
+                    display: "flex",
+                    flexDirection: "column",
+                    background: "#000000"
+                    }
+    const rowStyle1={
+                    width:"100%",
+                    height:"600px",
+                    background:"#000000",
+                    backgroundSize:"cover"
+                    }
+    const navStyle={
+                    margin:"0",
+                    padding:"10px",
+                    width:"100%",
+                    fontFamily:"Open Sans",
+                    borderTop:"2px solid #000000",
+                    borderBottom:"1px solid #000000",
+                    textAlign:"center",
+                    }
+      const linkStyle={
+                    textDecoration:"none",
+                    color:"#000000",
+                    fontSize:"2em",
+                    fontVariant:"small-caps",
+                    textAlign:"center",
+                    }
+
     return (
+
       <div>
+
         <Helmet title="About" meta={[ { name: 'description', content: 'Description of About' }]}/>
 
         <header>
-          <nav>
-            <Link to= "/"> Home </Link>
-            <Link to= "/About"> About </Link>
-          </nav>
+        <nav style={navStyle}>
+          <Link style={linkStyle} to= "/"> Home </Link>
+          <Link style={linkStyle} to= "/About"> About </Link>
+        </nav>
         </header>
+
         <main></main>
         <footer></footer>
       </div>
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -9,34 +9,29 @@ import Helmet from 'react-helmet';
 import {Link} from "react-router";
 export default class Home extends React.PureComponent {
   render() {
-    const divStyle={
-                    width:"60%",
-                    height:"800px",
-                    background: "#7fffd4",
-                    display:"flex",
-                    flexDirection:"column",
-
-                    }
-    const divStyle2={
-                    width:"40%",
-                    height:"800px",
-                    background:"#009976"
-                    }
-    const divStyle3={
+    const column1={
+                   width:"60%",
+                   height:"auto",
+                   textAlign:"center",
+                   color:"#ffffff",
+                   }
+        const box1={
+                   width:"100%",
+                   height:"700px",
+                   background:"url(http://h4z.it/Image/b8e6a3_hansnbecca.jpg)",
+                   backgroundSize:"cover",
+                   }
+     const column2={
+                   width:"40%",
+                   height:"auto",
+                   background:"#009976"
+                   }
+    const boxStyle2={
                     width:"100%",
-                    height:"200px",
+                    height:"400px",
                     background: "#000000",
                     color:"#ffffff",
                     textAlign:"center"
-                   }
-    const divStyle4={
-                    width:"85%",
-                    height:"300px",
-                    background:"url(http://h4z.it/Image/b8e6a3_hansnbecca.jpg)",
-                    backgroundSize:"cover",
-                    margin:"25px",
-                    textAlign:"center",
-                    color:"#ffffff"
                     }
     const mainStyle={
                     display: "flex",
@@ -44,69 +39,102 @@ export default class Home extends React.PureComponent {
                     backbround: "(#0000ff)"
                     }
     const postStyle={
-                      width:"100%",
-                      height:"100px",
-                      background:"#4dffc3",
-                      textAlign:"left justify",
-                      textIndent:"10px",
-                      color:"#ffffff"
-                      }
+                     width:"100%",
+                     height:"100px",
+                     background:"#d4efdf",
+                     textAlign:"left justify",
+                     textIndent:"10px",
+                     color:"#000000"
+                     }
     const postStyle2={
                       width:"100%",
                       height:"100px",
-                      background:"#33ffbb",
+                      background:"#A9DFBF",
                       textAlign:"left justify",
                       textIndent:"10px",
-                      color:"#fffffa"
+                      color:"#000000"
                       }
      const postStyle3={
                       width:"100%",
                       height:"100px",
-                      background:"#1affb2",
+                      background:"#7DCEA0",
                       textAlign:"left justify",
                       textIndent:"10px",
-                      color:"#fffffa"
+                      color:"#000000"
                       }
-  const titleStyle={
-                    fontFamily:"Times New Roman",
-                    fontStyle:"normal",
-                    fontSize:"4em"
-                   }
-  const titleStyle2={
-                    fontFamily:"Times New Roman",
-                    fontStyle:"normal",
-                    fontSize:"1em"
-                    }
-   const titleStyle3={
-                      fontFamily:"Times New Roman",
+     const titleStyle={
+                      fontFamily:"Open Sans",
                       fontStyle:"normal",
-                      fontSize:"5em"
+                      fontSize:"4em",
+                      paddingTop:"300px"
                       }
+    const titleStyle2={
+                      fontFamily:"Open Sans",
+                      fontStyle:"normal",
+                      fontSize:"1em"
+                      }
+    const titleStyle3={
+                      fontFamily:"Open Sans",
+                      fontStyle:"normal",
+                      fontSize:"4em",
+                      paddingTop:"80px"
+                      }
+ const animationStyle={
+                      width:"100%",
+                      height:"600px"
+                      }
+  const navStyle={
+                  margin:"0",
+                  padding:"10px",
+                  width:"100%",
+                  fontFamily:"Open Sans",
+                  borderTop:"2px solid #000000",
+                  borderBottom:"1px solid #000000",
+                textAlign:"center",
+                 }
+    const linkStyle={
+                  textDecoration:"none",
+                  color:"#000000",
+                  fontSize:"2em",
+                  fontVariant:"small-caps",
+                  textAlign:"center",
+                }
     return (
       <div>
 
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
 
         <header>
-          <nav>
-            <Link to= "/"> Home </Link>
-            <Link to= "/About"> About </Link>
+          <nav style={navStyle}>
+            <Link style={linkStyle} to= "/"> Home </Link>
+            <Link style={linkStyle} to= "/About"> About </Link>
           </nav>
         </header>
+
         <main style={mainStyle}>
 
-          <div style={divStyle}>
-            <div style={divStyle4}>
-             <div style={titleStyle}>Hans & Becca In The Wild</div></div>
-            <div style={postStyle}><div style={titleStyle2}>Daily Wild: Post 1...</div></div>
-            <div style={postStyle2}><div style={titleStyle2}>Daily Wild: Post 2...</div></div>
-            <div style={postStyle3}><div style={titleStyle2}>Daily Wild: Post 3...</div></div>
+          <div style={column1}>
+            <div style={box1}>
+              <div style={titleStyle}>Hans & Becca | In The Wild</div>
+            </div>
+            <div style={postStyle}>
+              <div style={titleStyle2}>Daily Wild | Post 1
+                <p/>Today we experienced the Redwoods of Northern California. These huge trees stretching as far up as the eye can see are quite the site to see!
+              </div></div>
+            <div style={postStyle2}><div style={titleStyle2}>Daily Wild | Post 2...</div></div>
+            <div style={postStyle3}><div style={titleStyle2}>Daily Wild | Post 3...</div></div>
           </div>
-          <div style={divStyle2}>
-            <div style={divStyle3}><div style={titleStyle3}>Our Story</div></div>
+          <div style={column2}>
+            <div style={boxStyle2}>
+              <div style={titleStyle3}>Grow old along with me!<br/>
+               The best is yet to be.
+              </div>
+            </div>
+              <img style={animationStyle} src="https://media.giphy.com/media/3o7qE4p0MDWCMWs52M/giphy.gif"/>
           </div>
         </main>
-        <footer></footer>
+        <footer>
+        </footer>
       </div>
     );
   }
